1273ad9e1c159850a5941fdc8c8ad35513f29435
Jomjam <jomjam.kusumaj@gmail.com>
2025-03-17 14:11:17 +0700

    add font

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+
+interface NavBarCursorProps {
+  left: number;
+  width: number;
+  opacity: number;
+}
+const NavigationBar = () => {
+  const [position, setPosition] = React.useState<NavBarCursorProps>({
+    left: 0,
+    width: 0,
+    opacity: 0,
+  });
+  return (
+    <div
+      onMouseLeave={() => setPosition((prev) => ({ ...prev, opacity: 0 }))}
+      className="fixed top-10 left-0 right-0"
+    >
+      <ul className="z-10 mx-auto flex w-fit rounded-full backdrop-blur-lg border-1 border-white/10 bg-white/5 p-1">
+        <Tab setPosition={setPosition}>Home</Tab>
+        <Tab setPosition={setPosition}>Work</Tab>
+        <Tab setPosition={setPosition}>About</Tab>
+        <Tab setPosition={setPosition}>Contact</Tab>
+        <Cursor position={position} />
+      </ul>
+      {/* <Lamp /> */}
+    </div>
+  );
+};
+
+export default NavigationBar;
+
+const Tab = ({
+  children,
+  setPosition,
+}: {
+  children: React.ReactNode;
+  setPosition: React.Dispatch<React.SetStateAction<NavBarCursorProps>>;
+}) => {
+  const ref = React.useRef<HTMLLIElement>(null);
+  return (
+    <li
+      ref={ref}
+      onMouseEnter={() => {
+        if (!ref.current) return;
+        const { width } = ref.current.getBoundingClientRect();
+        setPosition({
+          left: ref.current.offsetLeft,
+          width,
+          opacity: 1,
+        });
+      }}
+      className="relative block px-3 py-1.5 text-xs uppercase text-white font-['Helvetica'] font-extralight"
+    >
+      {children}
+    </li>
+  );
+};
+
+const Cursor = ({ position }: { position: NavBarCursorProps }) => {
+  return (
+    <motion.li
+      animate={{
+        left: position.left,
+        width: position.width,
+        opacity: position.opacity,
+      }}
+      className="absolute z-0 h-7 bg-white/10 rounded-full"
+    />
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Gabarito } from "next/font/google";
 import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+import NavigationBar from "./components/NavBar";
 
 export const metadata: Metadata = {
   title: "Kusuma Resume",
@@ -24,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavigationBar />
+        {children}
+      </body>
     </html>
   );
 }
