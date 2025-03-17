a4c0f688edf27cbaaa9ad2575f066f25b85a5583
Jomjam <jomjam.kusumaj@gmail.com>
2025-03-17 14:40:25 +0700

    add png

--- app/sections/Landing.tsx
+++ app/sections/Landing.tsx
@@ -11,6 +11,11 @@ const Interlude = () => {
           by making this website looks{" "}
           <span className="font-['Pattaya']! text-6xl">cool</span>
         </h1>
+        <div className="flex justify-center items-center text-xl text-center">
+          <span>Hello, I’m Kusuma Jomjam</span>
+          <button className="h-20 w-20 bg-[url('/kusuma-profile.png')] bg-contain border-1 rounded-full m-4  cursor-pointer"></button>
+          <span>a Software Engineer</span>
+        </div>
       </div>
     </div>
   );
