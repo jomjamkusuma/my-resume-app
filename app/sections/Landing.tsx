04759398a24a9b1a6c1f55603078ef63ce167b84
Jomjam <jomjam.kusumaj@gmail.com>
2025-03-17 13:46:41 +0700

    add  landing and delete unused

---
+++ app/sections/Landing.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Interlude = () => {
+  return (
+    <div className="flex h-screen bg-[url('/landing-background.svg')] bg-cover bg-center justify-center items-center">
+      <div>
+        <h1 className="text-5xl text-white font-bold text-center pt-4">
+          I just need a new job
+        </h1>
+        <h1 className="text-5xl text-white font-bold text-center pt-4">
+          by making this website looks{" "}
+          <span className="font-['Pattaya']! text-6xl">cool</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Interlude;
